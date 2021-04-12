c2f981f8b9eef6186059dbf427239d38242d2142
Yunus <yunusadbulqudus1@gmail.com>
2021-04-12 13:54:11 +0100

    order status

--- resources/js/store/modules/shop.js
+++ resources/js/store/modules/shop.js
@@ -107,47 +107,6 @@ export default {
                 }
             return str.join("&");
         },
-        getOrderStatusColor({state}, status){
-
-            switch (status){
-
-                case 'Initiated': {
-
-                    return '-primary'
-
-                }
-                case 'Processing': {
-
-                    return '-blue'
-
-                }
-                case 'Shipped': {
-
-                    return '-warning'
-
-                }
-                case 'Delivered': {
-
-                    return '-success'
-
-                }
-                case 'Cancelled': {
-
-                    return '-danger'
-
-                }
-
-                default: {
-
-                    return '-secondary'
-
-                }
-
-
-            }
-
-        }
-
     },
     mutations: {
 
