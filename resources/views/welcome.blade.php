dbd0859b47d05f3b653c8025026bf1e4023ecddc
Yunus <yunusadbulqudus1@gmail.com>
2021-04-24 23:55:15 +0100

    changed config

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -139,7 +139,7 @@ Home
                 <div class="fw-section rounded padding-top-4x padding-bottom-4x" style="background-image: url(img/banners/home02.jpg);"><span class="overlay rounded" style="opacity: .35;"></span>
                     <div class="text-center">
                         <h3 class="display-4 text-normal text-white text-shadow mb-1">Old Collection</h3>
-                        <h2 class="display-2 text-bold text-white text-shadow">HUGE SALE!!!f..</h2>
+                        <h2 class="display-2 text-bold text-white text-shadow">HUGE SALE!!!</h2>
                         <h4 class="d-inline-block h2 text-normal text-white text-shadow border-default border-left-0 border-right-0 mb-4">at our outlet stores</h4><br><a class="btn btn-primary margin-bottom-none" href="contacts.html">Locate Stores</a>
                     </div>
                 </div>
