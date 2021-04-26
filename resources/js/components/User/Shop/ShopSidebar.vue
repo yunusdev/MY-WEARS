ffa8fc23170a3384a88f5975bb25f9839c4a8b1b
Yunus <yunusadbulqudus1@gmail.com>
2021-04-26 15:36:49 +0100

    cleanup

--- resources/js/components/User/Shop/ShopSidebar.vue
+++ resources/js/components/User/Shop/ShopSidebar.vue
@@ -4,10 +4,11 @@
             <h3 class="widget-title">Shop Categories</h3>
             <ul>
                 <li :class="{'active': category && category.slug === cat.slug}" v-for="cat in categories" :key="cat.id" class="has-children mb-3 expanded">
-                    <a :href="`/category/${cat.slug}/products`">{{ cat.name }} <span>(1138)</span></a>
+                    <a :href="`/category/${cat.slug}/products`">{{ cat.name }}
+                    </a>
                     <ul>
                         <li class="mt-2" :class="{'active': sub_category && sub_category.slug === sub_cat.slug}" v-for="sub_cat in cat.sub_categories" :key="sub_cat.id">
-                            <a class="" :href="`/sub-category/${sub_cat.slug}/products`">{{sub_cat.name}} <span>(508)</span></a>
+                            <a class="" :href="`/sub-category/${sub_cat.slug}/products`">{{sub_cat.name}}</a>
                         </li>
                     </ul>
                 </li>
