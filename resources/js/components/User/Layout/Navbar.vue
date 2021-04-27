c72f254f5fb8b9186c39ee23163ad7d1d39913cd
Yunus <yunusadbulqudus1@gmail.com>
2021-04-27 15:50:37 +0100

    made some changes

--- resources/js/components/User/Layout/Navbar.vue
+++ resources/js/components/User/Layout/Navbar.vue
@@ -72,27 +72,13 @@
         <div class="topbar">
             <div class="topbar-column pb-2 pt-2">
                 <div class="lang-currency-switcher-wrap">
-                    <a class="" href="tel:00331697720"><span class="font-weight-bold">PHONE:</span> {{ config.contact_phone }}</a>
-                        <a class="" href="tel:00331697720"><span class="font-weight-bold">MAIL:</span> {{ config.contact_email }}</a>
-<!--                    <a class="" href="mailto:support@unishop.com">&nbsp; support@unisp.com</a>-->
-                    <!--                    <div class="lang-currency-switcher dropdown-toggle"><span class="language"><img alt="English" src="/img/flags/GB.png"></span><span class="currency">$ USD</span></div>-->
-                    <!--                    <div class="dropdown-menu">-->
-                    <!--                        <div class="currency-select">-->
-                    <!--                            <select class="form-control form-control-rounded form-control-sm">-->
-                    <!--                                <option value="usd">$ USD</option>-->
-                    <!--                                <option value="usd">€ EUR</option>-->
-                    <!--                                <option value="usd">£ UKP</option>-->
-                    <!--                                <option value="usd">¥ JPY</option>-->
-                    <!--                            </select>-->
-                    <!--                        </div><a class="dropdown-item" href="#"><img src="/img/flags/FR.png" alt="Français">Français</a><a class="dropdown-item" href="#"><img src="img/flags/DE.png" alt="Deutsch">Deutsch</a><a class="dropdown-item" href="#"><img src="img/flags/IT.png" alt="Italiano">Italiano</a>-->
-                    <!--                    </div>-->
+                    <a class="" :href="'tel:'+ config.contact_phone"><span class="font-weight-bold">PHONE:</span> {{ config.contact_phone }}</a>
+                    <a class="" :href="'mailto:'+config.contact_email"><span class="font-weight-bold">MAIL:</span> {{ config.contact_email }}</a>
+                    <a v-if="!user" class="hidden-md-down" href="/login"><i class=""></i>&nbsp; Login</a>
+                    <a v-if="!user" class="hidden-md-down" href="/register"><i class=""></i>&nbsp; Register</a>
                 </div>
-                <a v-if="!user" class="hidden-md-down" href="/login"><i class=""></i>&nbsp; Login</a>
-                <a v-if="!user" class="hidden-md-down" href="/register"><i class=""></i>&nbsp; Register</a>
             </div>
             <div class="topbar-column">
-<!--                <a class="hidden-md-down" href="mailto:support@unishop.com"><i class="icon-mail"></i>&nbsp; support@unishop.com</a>-->
-<!--                <a class="hidden-md-down" href="tel:00331697720"><i class="icon-bell"></i>&nbsp; 00 33 169 7720</a>-->
                 <a class="social-button sb-facebook shape-none sb-dark" href="#" target="_blank"><i class="socicon-facebook"></i></a><a class="social-button sb-twitter shape-none sb-dark" href="#" target="_blank"><i class="socicon-twitter"></i></a><a class="social-button sb-instagram shape-none sb-dark" href="#" target="_blank"><i class="socicon-instagram"></i></a><a class="social-button sb-pinterest shape-none sb-dark" href="#" target="_blank"><i class="socicon-pinterest"></i></a>
             </div>
         </div>
@@ -203,9 +189,9 @@ export default {
     },
     async mounted() {
 
-        await this.getUserCartItems()
-        await this.getCategories({})
-        await this.getConfig({})
+        this.getUserCartItems()
+        this.getCategories({})
+        this.getConfig({})
 
     },
 
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -19,40 +19,27 @@
     <link rel="apple-touch-icon" sizes="152x152" href="touch-icon-ipad.png">
     <link rel="apple-touch-icon" sizes="180x180" href="touch-icon-iphone-retina.png">
     <link rel="apple-touch-icon" sizes="167x167" href="touch-icon-ipad-retina.png">
-    <!-- Vendor Styles including: Bootstrap, Font Icons, Plugins, etc.-->
     <link rel="stylesheet" media="screen" href="{{asset('css/vendor.min.css')}}">
     <link rel="stylesheet" media="screen" href="{{asset('css/base.css')}}">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
-    <!-- Main Template Styles-->
     <link id="mainStyles" rel="stylesheet" media="screen" href="{{asset('css/styles.min.css')}}">
-    <!-- Modernizr-->
     <script src="{{asset('js/modernizr.min.js')}}"></script>
     @yield('styles')
 </head>
 <!-- Body-->
 <body>
-
     <div id="app">
-
-        {{--@include('layouts.header')--}}
         <nav-bar raw_url="{{$url}}" raw_user="{{auth()->user()}}"></nav-bar>
         <div class="offcanvas-wrapper">
-            <!-- Page Content-->
             @yield('content')
-
             <user-footer raw_user="{{auth()->user()}}"></user-footer>
         </div>
-
     </div>
-<!-- Back To Top Button--><a class="scroll-to-top-btn" href="#"><i class="icon-arrow-up"></i></a>
-<!-- Backdrop-->
+<a class="scroll-to-top-btn" href="#"><i class="icon-arrow-up"></i></a>
 <div class="site-backdrop"></div>
-<!-- JavaScript (jQuery) libraries, plugins and custom scripts-->
-    <script src="{{asset('js/app.js')}}"></script>
-
-    <script src="{{asset('js/vendor.min.js')}}"></script>
+<script src="{{asset('js/app.js')}}"></script>
+<script src="{{asset('js/vendor.min.js')}}"></script>
 <script src="{{asset('js/scripts.min.js')}}"></script>
-
 </body>
 </html>
